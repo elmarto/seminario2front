59092111095c9b898afe3de124b6b0a00799fd34
Martin Sacco <sacco.martin@gmail.com>
2018-05-19 16:18:05 -0300

    Remove unused routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: 'institutional',
-    loadChildren: 'app/institutional/institutional.module#InstitutionalModule'
-  },
-  {
-    path: 'feeds',
-    loadChildren: 'app/feeds/feeds.module#FeedsModule'
-  }
   // { path: '404', redirectTo: '' },
   // { path: '**', redirectTo: '' }
 ];
